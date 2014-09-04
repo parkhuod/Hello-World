798bb6cce2b9ddcadccf483d88ba8b1c841a1ebe
parkhuod <parkhuod@dukes.jmu.edu>
2014-09-04 13:31:55 -0400

    Function changes

---
+++ Heythere.R
@@ -0,0 +1,3 @@
+Hello <- function(){
+writeLines("Hey There")
+}
---
+++ myRfunction.R
@@ -0,0 +1,44 @@
+
+R version 3.1.1 (2014-07-10) -- "Sock it to Me"
+Copyright (C) 2014 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin13.1.0 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[R.app GUI 1.65 (6784) x86_64-apple-darwin13.1.0]
+
+[History restored from /Users/oliviaparkhurst2/.Rapp.history]
+
+> function (){
++ 
++ greeting <- "Bonjour Earth"
++ frame <- data.frame(Bonjour-c(4,5,6),Earth=c(6,7,8))
++ vector <- c(8,9,7)
++ list<- list(,2,frame,4,5)
++ new_list <-c(list,6)
++ closing <- "I'm finished"
++ return(list(greeting,vector,frame,new_list,closing))
++ }
+function (){
+
+greeting <- "Bonjour Earth"
+frame <- data.frame(Bonjour-c(4,5,6),Earth=c(6,7,8))
+vector <- c(8,9,7)
+list<- list(,2,frame,4,5)
+new_list <-c(list,6)
+closing <- "I'm finished"
+return(list(greeting,vector,frame,new_list,closing))
+}
+> 
