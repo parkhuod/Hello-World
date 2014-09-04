c9570501835577938b94da625f77094230edc48f
parkhuod <parkhuod@dukes.jmu.edu>
2014-09-04 13:28:04 -0400

    changes

---
+++ 1.5fn.R
@@ -0,0 +1,9 @@
+function (){
+greeting <- "bonjour earth"
+frame <- data.frame(bonjour=c(4,5,6),Earth=c(6,7,8))
+vector <- c(8,9,7)
+list<- list(1,2,frame,4,5)
+new_list <-c(list,6)
+closing <- "I'm finished"
+return(list(greeeting,vector,frame,new_list,closing))
+}
